88eb4dd4e0b8976a2e05b052d97b5796f48dd17f
Ghanshyam_1612 <ghanshyamsolanki1612@gmail.com>
2024-04-08 15:56:55 +0530

    Sandbox Initial

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <router-view/>
-  </div>
-</template>
-
-"At [Company Name], we redefine possibilities through tailored IT solutions—seamless, secure, and crafted for your success in the digital realm."
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,53 +1,76 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{vue,html,js}"],
+  content: [
+    "./dist/**/*.html",
+  ],
+  darkMode: 'class',
   theme: {
-    extend: {
-      fontFamily: {
-        "Open-Sans": ['Open Sans', 'sans-serif'],
-        "Glory": ['Glory', 'sans-serif'],
-        "Montserrat": ['Montserrat', 'sans-serif'],
-        'Poppins': ['Poppins', 'sans-serif']
-      },
-      backgroundImage: {
-        'companyBg': "url('~@/assets/video/Iconic Online Marketing Background.mp4')",
-        'serviceBg': "url('~@/assets/image/serviceBg.jpg')",
-        'interviewBg': "url('~@/assets/image/job-interview.jpg')",
-        'webDevelopment': "url('~@/assets/image/web-programming.png')",
-        'apiDevelopment': "url('~@/assets/image/api.png')",
-        'laravelUpgrades': "url('~@/assets/image/upgrade.png')",
-        'codeOptimization': "url('~@/assets/image/source-code.png')",
-        'verifyImg': "url('~@/assets/image/verify.png')",
-        'supportImg': "url('~@/assets/image/customer-service.png')",
-        'skillImg': "url('~@/assets/image/skill.png')",
-        'choiceImg': "url('~@/assets/image/choice.png')",
-        'facebook': "url('~@/assets/image/facebook.png')",
-        'instagram': "url('~@/assets/image/instagram.png')",
-        'twitter': "url('~@/assets/image/twitter.png')",
-        'linkedin': "url('~@/assets/image/linkedin.png')",
-        'user1': "url('~@/assets/image/user-1.jpg')",
-        'user2': "url('~@/assets/image/user-2.jpg')",
-        'user3': "url('~@/assets/image/user-3.jpg')",
-        'ratingStar': "url('~@/assets/image/star.png')",
-        'vision': "url('~@/assets/image/Vision.png')",
-        'mission': "url('~@/assets/image/Mission.png')",
-        'values': "url('~@/assets/image/Values.png')",
-
-        // Tution Mate
-        'tution01': "url('~@/assets/portfolio/tutionmate/01.jpeg')",
-        'tution02': "url('~@/assets/portfolio/tutionmate/02.jpeg')",
-        'tution03': "url('~@/assets/portfolio/tutionmate/03.jpeg')",
-      },
-      animation: {
-        'fade-in': 'fadeIn 1s ease-in-out', // Define the animation
-        'txtfade-in': 'fadeIn 2s ease-in-out',
-      },
-     
+    fontFamily: {
+      'IBMPlexSerif': ['IBM Plex Serif', 'serif'],
+      'SpaceGrotesk': ['Space Grotesk', 'sans-serif'],
+      'Manrope': ['Manrope', 'sans-serif'],
+      'Unicons': ['Unicons'],
+      'Monospace': ['SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace'],
+      'Custom': ['Custom'],
+      'DMSerif': ['DM Serif Display'],
+      'SansSerif': ['sans-serif'],
+      'THICCCBOI': ['THICCCBOI', 'sans-serif'],
+      'Urbanist': ['Urbanist', 'sans-serif'],
     },
-    // safelist: ['animate-fade-in_1s_ease-in-out'], // Add the animation class to the safelist
+    container: {
+      center: true,
+      padding: '15px',
+    },
+    extend: {
+      screens: {
+        'xxl': {
+          'min': '1400px',
+        },
+        'xl': {
+          'min': '1200px',
+        },
+        'lg': {
+          'min': '992px',
+          'max': '1199.98px'
+        },
+        'md': {
+          'min': '768px',
+          'max': '991.98px'
+        },
+        'sm': {
+          'min': '576px',
+          'max': '767.98px'
+        },
+        'xsm': {
+          'max': '575.98px'
+        },
+      },
+    },
   },
   plugins: [
-    // require('@tailwindcss/animation'), // Include the animation plugin to handle the animation classes.
-  ],
+    function ({
+    addComponents
+  }) {
+    addComponents({
+      '.container': {
+        maxWidth: '100%',
+        '@screen sm': {
+          maxWidth: '540px',
+        },
+        '@screen md': {
+          maxWidth: '720px',
+        },
+        '@screen lg': {
+          maxWidth: '960px',
+        },
+        '@screen xl': {
+          maxWidth: '1140px',
+        },
+        '@screen xxl': {
+          maxWidth: '1320px',
+        },
+      }
+    })
+  }],
 }
-
